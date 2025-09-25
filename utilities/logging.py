757be762e265410225055b816665e21490ea8ff9
Jonathan Thorne <jon@jonthorne.com>
2025-09-24 22:58:13 -0400

    add seperate log for dev bot

--- utilities/logging.py
+++ utilities/logging.py
@@ -1,10 +1,16 @@
 import logging
 import logging.config
 import utilities
+from utilities import get_env
 from utilities import Config
 
 config = Config(True) # EDIT THIS LATER TO NOT HAVE A BOOLEAN
-logging.config.fileConfig("logging.conf")
+token = get_env.discord_token()
+if token.endswith("B2M"):
+    logfile = "chhbot-dev.log"
+else:
+    logfile = "chhbot.log"
+logging.config.fileConfig("logging.conf", defaults={'logfilename': logfile})
 
 logger = logging.getLogger()
 if config.is_dev:
