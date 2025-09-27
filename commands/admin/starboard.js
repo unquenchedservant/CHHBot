179a7640b9297043d3311a440b0146ea3f36f1d8
Jonathan Thorne <jon@jonthorne.com>
2025-09-27 12:48:52 -0400

    Update starboard command replies to be ephemeral

--- commands/admin/starboard.js
+++ commands/admin/starboard.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, channelMention, PermissionFlagsBits } = require('discord.js');
+const { SlashCommandBuilder, channelMention, PermissionFlagsBits, MessageFlags } = require('discord.js');
 const { StarboardSettingsDB } = require('../../db/starboard');
 
 const starboardsettings_db = new StarboardSettingsDB();
@@ -29,12 +29,12 @@ module.exports = {
     async execute(interaction) {
         if (interaction.options.getSubcommand() === 'setthreshold'){
             const threshold = interaction.options.getInteger('threshold');
-            if (!await starboardsettings_db.check(guildId)){
+            if (!await starboardsettings_db.check(interaction.guildId)){
                 await starboardsettings_db.add(interaction.guildId, 0, threshold);
             }else{
                 await starboardsettings_db.updateThreshold(interaction.guildId, threshold);
             }
-            await interaction.reply(`Starboard threshold set to ${threshold}`);
+            await interaction.reply({ content: `Starboard threshold set to ${threshold}`, flags: MessageFlags.Ephemeral});
         }else if(interaction.options.getSubcommand() === 'getthreshold'){
             var threshold = 0
             if (!await starboardsettings_db.check(interaction.guildId)){
@@ -44,7 +44,7 @@ module.exports = {
                 threshold = await starboardsettings_db.getThreshold(interaction.guildId)
             }
             
-            await interaction.reply(`Current Starboard threshold is ${threshold}`)
+            await interaction.reply({ content:`Current Starboard threshold is ${threshold}`, flags: MessageFlags.Ephemeral})
         }
     }
 };
