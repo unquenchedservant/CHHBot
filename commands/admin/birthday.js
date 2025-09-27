a0bc10f998ede10021da6f3c487f55478e1fc5f4
Jonathan Thorne <jon@jonthorne.com>
2025-09-27 15:33:24 -0400

    Port birthday admin command to JS

---
+++ commands/admin/birthday.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } = require('discord.js')
+const { BirthdayDB } = require('../../db/birthday')
+
+const birthday_db = new BirthdayDB();
+
+const data = new SlashCommandBuilder()
+    .setName('checkbirthday')
+    .setDescription("check any user to see if they have a birthday set")
+    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+    .addUserOption(option =>
+        option
+        .setName("user")
+        .setDescription("User to check")
+        .setRequired(true)
+    )
+
+module.exports = {
+    data, 
+    async execute(interaction){
+        let birthday = birthday_db.get(interaction.options.getUser('user').id)
+        if (birthday == [0,0]){
+            await interaction.reply({ content: "User does not have a birthday set", flags: MessageFlags.Ephemeral})
+        }else{
+            await interaction.reply({ content: `${interaction.options.getUser('user').name} birthday is set to ${birthday[0]}/${birthday[1]}`, flags: MessageFlags.Ephemeral})
+        }
+    }
+}
