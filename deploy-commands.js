8961419c0b98b4f1fbfd442fa6b043c4316b0bde
Jonathan Thorne <jon@jonthorne.com>
2025-09-27 17:34:21 -0400

    Update existing console log statements to use new logger

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,6 +2,7 @@ const { REST, Routes } = require('discord.js');
 const dotenv = require('dotenv');
 const fs = require('node:fs');
 const path = require('node:path');
+const logger = require('utility/logger');
 
 dotenv.config();
 
@@ -24,7 +25,7 @@ for (const folder of commandFolders) {
         if ('data' in command && 'execute' in command) {
             commands.push(command.data.toJSON());
         } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            logger.info(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
         }
     }
 }
@@ -33,15 +34,15 @@ const rest = new REST().setToken(token);
 
 (async () => {
     try {
-        console.log(`Started refreshing ${commands.length} application (/).`);
+        logger.info(`Started refreshing ${commands.length} application (/).`);
 
         const data = await rest.put(
             Routes.applicationGuildCommands(clientId, guildId),
             { body: commands },
         );
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+        logger.info(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
-        console.error(error);
+        logger.error(error);
     }
 })();
