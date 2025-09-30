2490658563c77cee79cbc2be1f35fcc50cd793fe
Jonathan Thorne <jon@jonthorne.com>
2025-09-29 22:15:57 -0400

    lint rool files

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,33 @@
 const js = require('@eslint/js');
 
 module.exports = [
-  js.configs.recommended,
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-    },
-    rules: {
-      'arrow-spacing': ['warn', { before: true, after: true }],
-      'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
-      'comma-dangle': ['error', 'always-multiline'],
-      'comma-spacing': 'error',
-      'comma-style': 'error',
-      curly: ['error', 'multi-line', 'consistent'],
-      'dot-location': ['error', 'property'],
-      'handle-callback-err': 'off',
-      indent: ['error', 'tab'],
-      'keyword-spacing': 'error',
-      'max-nested-callbacks': ['error', { max: 4 }],
-      'max-statements-per-line': ['error', { max: 2 }],
-      'no-console': 'off',
-      'no-empty-function': 'error',
-      'no-floating-decimal': 'error',
-      'no-inline-comments': 'error',
-      'no-lonely-if': 'error',
-      'no-multi-spaces': 'error',
-      'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1, maxBOF: 0 }],
-      'no-shadow': ['error', { allow: ['err', 'resolve', 'reject']}],
-      'no-trailing-spaces': ['error'],
+	js.configs.recommended,
+	{
+		languageOptions: {
+			ecmaVersion: 'latest',
+		},
+		rules: {
+			'arrow-spacing': ['warn', { before: true, after: true }],
+			'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+			'comma-dangle': ['error', 'always-multiline'],
+			'comma-spacing': 'error',
+			'comma-style': 'error',
+			curly: ['error', 'multi-line', 'consistent'],
+			'dot-location': ['error', 'property'],
+			'handle-callback-err': 'off',
+			indent: ['error', 'tab'],
+			'keyword-spacing': 'error',
+			'max-nested-callbacks': ['error', { max: 4 }],
+			'max-statements-per-line': ['error', { max: 2 }],
+			'no-console': 'off',
+			'no-empty-function': 'error',
+			'no-floating-decimal': 'error',
+			'no-inline-comments': 'error',
+			'no-lonely-if': 'error',
+			'no-multi-spaces': 'error',
+			'no-multiple-empty-lines': ['error', { max: 2, maxEOF: 1, maxBOF: 0 }],
+			'no-shadow': ['error', { allow: ['err', 'resolve', 'reject'] }],
+			'no-trailing-spaces': ['error'],
 			'no-var': 'error',
 			'no-undef': 'off',
 			'object-curly-spacing': ['error', 'always'],
@@ -45,6 +45,6 @@ module.exports = [
 			'space-unary-ops': 'error',
 			'spaced-comment': 'error',
 			yoda: 'error',
-    },
-  }
-]
+		},
+	},
+];
