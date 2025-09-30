da68c325ab09717ccef591c3a40394c68c5d7245
Jonathan Thorne <jon@jonthorne.com>
2025-09-29 22:13:46 -0400

    lint DB files

--- db/starboard.js
+++ db/starboard.js
@@ -1,71 +1,71 @@
 const Database = require('./database');
 
 class StarboardSettingsDB extends Database {
-    constructor() {
-        super();
-        this.create();
-    }
+	constructor() {
+		super();
+		this.create();
+	}
 
-    async create() {
-        await this.execute(`CREATE TABLE IF NOT EXISTS starboardsettings
+	async create() {
+		await this.execute(`CREATE TABLE IF NOT EXISTS starboardsettings
             (GUILDID INTEGER NOT NULL,
             STARBOARDCHANNEL INTEGER NOT NULL,
             STARBOARDTHRESHOLD INTEGER NOT NULL)`);
-    }
+	}
 
-    async add(guildId, starboardChannel, starboardThreshold){
-        await this.execute(`INSERT INTO starboardsettings
+	async add(guildId, starboardChannel, starboardThreshold) {
+		await this.execute(`INSERT INTO starboardsettings
             (GUILDID, STARBOARDCHANNEL, STARBOARDTHRESHOLD)
             VALUES (${guildId}, ${starboardChannel}, ${starboardThreshold})`);
-    }
+	}
 
-    async check(guildId) {
-        const data = await this.execute(`SELECT * FROM starboardsettings WHERE GUILDID=${guildId}`);
-        return this.checkLen(data);
-    }
+	async check(guildId) {
+		const data = await this.execute(`SELECT * FROM starboardsettings WHERE GUILDID=${guildId}`);
+		return this.checkLen(data);
+	}
 
-    async updateChannel(guildId, starboardChannel){
-        console.log(`UPDATE CHANNEL ID: ${starboardChannel}`)
-        await this.execute(`UPDATE starboardsettings
+	async updateChannel(guildId, starboardChannel) {
+		console.log(`UPDATE CHANNEL ID: ${starboardChannel}`);
+		await this.execute(`UPDATE starboardsettings
             SET STARBOARDCHANNEL=${starboardChannel}
             WHERE GUILDID=${guildId}`);
-    }
+	}
 
-    async updateThreshold(guildId, starboardThreshold) {
-        await this.execute(`UPDATE starboardsettings
+	async updateThreshold(guildId, starboardThreshold) {
+		await this.execute(`UPDATE starboardsettings
             SET STARBOARDTHRESHOLD=${starboardThreshold}
             WHERE GUILDID=${guildId}`);
-    }
+	}
 
-    async getSettings(guildId){
-        const data = await this.execute(`SELECT STARBOARDCHANNEL, STARBOARDTHRESHOLD
+	async getSettings(guildId) {
+		const data = await this.execute(`SELECT STARBOARDCHANNEL, STARBOARDTHRESHOLD
             FROM starboardsettings WHERE GUILDID=${guildId}`);
-        return data[0];
-    }
+		return data[0];
+	}
 
-    async getThreshold(guildId){
-        const data = await this.execute(`SELECT STARBOARDTHRESHOLD
+	async getThreshold(guildId) {
+		const data = await this.execute(`SELECT STARBOARDTHRESHOLD
             FROM starboardsettings WHERE GUILDID=${guildId}`);
-        return data[0].STARBOARDTHRESHOLD;
-    }
+		return data[0].STARBOARDTHRESHOLD;
+	}
 
-    async getChannel(guildId){
-        const data = await this.execute(`SELECT STARBOARDCHANNEL
+	async getChannel(guildId) {
+		const data = await this.execute(`SELECT STARBOARDCHANNEL
             FROM starboardsettings WHERE GUILDID=${guildId}`);
-        console.dir(data[0], {depth: null});
-        console.log(`DATA IS ${data[0].STARBOARDCHANNEL.toString()}`)
-        return data[0].STARBOARDCHANNEL.toString();
-    }
+		console.dir(data[0], { depth: null });
+		console.log(`DATA IS ${data[0].STARBOARDCHANNEL.toString()}`);
+		return data[0].STARBOARDCHANNEL.toString();
+	}
 
-    async remove(guildId){
-        await this.execute(`DELETE FROM starboardsettings WHERE GUILDID=${guildId}`);
-    }
+	async remove(guildId) {
+		await this.execute(`DELETE FROM starboardsettings WHERE GUILDID=${guildId}`);
+	}
 
-    async drop() {
-        await this.execute('DROP TABLE starboardsettings');
-    }
+	async drop() {
+		await this.execute('DROP TABLE starboardsettings');
+	}
 }
 
 module.exports = {
-    StarboardSettingsDB
+	StarboardSettingsDB,
 };
