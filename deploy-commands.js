b4871b0da5413aec097c9306431ba79837b00f98
Jonathan Thorne <jon@jonthorne.com>
2025-09-29 22:15:57 -0400

    lint rool files

--- deploy-commands.js
+++ deploy-commands.js
@@ -16,47 +16,51 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            if (command.data.toJSON){
-                 commands.push(command.data.toJSON());
-            }else if(command.data.slash){
-                 commands.push(command.data.slash);
-                if(command.data.context){
-                    commands.push(command.data.context);
-                }
-            }else{
-                commands.push(command.data)
-            }
-        } else {
-            logger.info(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
-    
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			if (command.data.toJSON) {
+				commands.push(command.data.toJSON());
+			}
+			else if (command.data.slash) {
+				commands.push(command.data.slash);
+				if (command.data.context) {
+					commands.push(command.data.context);
+				}
+			}
+			else {
+				commands.push(command.data);
+			}
+		}
+		else {
+			logger.info(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+
 }
 logger.info('Deploying commands:');
 commands.forEach(cmd => {
-    logger.info(`- ${cmd.name} (${cmd.type})`);
+	logger.info(`- ${cmd.name} (${cmd.type})`);
 });
 
 const rest = new REST().setToken(token);
 
 (async () => {
-    try {
-        logger.info(`Started refreshing ${commands.length} application (/).`);
+	try {
+		logger.info(`Started refreshing ${commands.length} application (/).`);
 
-        const data = await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
+		const data = await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
 
-        logger.info(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-        logger.error(error);
-    }
+		logger.info(`Successfully reloaded ${data.length} application (/) commands.`);
+	}
+	catch (error) {
+		logger.error(error);
+	}
 })();
--- index.js
+++ index.js
@@ -19,44 +19,47 @@ const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
 
-        if ('data' in command && 'execute' in command){
-            client.commands.set(command.data.name, command);
-        } else {
-            logger.warn(`The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		}
+		else {
+			logger.warn(`The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
 }
 
 client.once(Events.ClientReady, readyClient => {
-    logger.info(`Ready! Logged in as ${readyClient.user.tag}`);
+	logger.info(`Ready! Logged in as ${readyClient.user.tag}`);
 });
 
 client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand() && !interaction.isMessageContextMenuCommand() ) return;
-    
-    const command = interaction.client.commands.get(interaction.commandName);
+	if (!interaction.isChatInputCommand() && !interaction.isMessageContextMenuCommand()) return;
 
-    if (!command) {
-        logger.error(`No command matching ${interaction.commandName} was found.`);
-        return;
-    }
+	const command = interaction.client.commands.get(interaction.commandName);
 
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        logger.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-        } else {
-            await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
-        }
-    }
+	if (!command) {
+		logger.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	}
+	catch (error) {
+		logger.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+		else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
 });
 
 client.login(token);
