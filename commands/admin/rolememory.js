9eadce0f723c2482c58eb36cd724721761987913
Jonathan Thorne <jon@jonthorne.com>
2025-09-29 22:12:49 -0400

    lint admin commands

--- commands/admin/rolememory.js
+++ commands/admin/rolememory.js
@@ -5,38 +5,39 @@ const logger = require('../../utility/logger');
 const rolememory_db = new RoleMemoryDB();
 
 const data = new SlashCommandBuilder()
-     .setName('rolememory')
-     .setDescription("Role Memory related settings")
-     .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
-     .addSubcommand(subcommand => 
-        subcommand
-        .setName("toggle")
-        .setDescription("Toggle Role Memory on/off")
-     )
-     .addSubcommand(subcommand =>
-        subcommand
-        .setName("check")
-        .setDescription("Check the current state of Role Memory")
-     )
+	.setName('rolememory')
+	.setDescription('Role Memory related settings')
+	.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName('toggle')
+			.setDescription('Toggle Role Memory on/off'),
+	)
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName('check')
+			.setDescription('Check the current state of Role Memory'),
+	);
 
 module.exports = {
-    data,
-    async execute(interaction){
-        if (interaction.options.getSubcommand() === "toggle"){
-            logger.info(`'/rolememory toggle' was called by ${interaction.user.tag}`)
-            let status = await rolememory_db.check(interaction.guildId)
-            let msg = status == 1
-            ? "Role memory has been turned off for this server"
-            : "Role memory has been turned on for this server"
-            await rolememory_db.toggle(interaction.guildId)
-            await interaction.reply({ content: msg, flags: MessageFlags.Ephemeral })
-        }else if(interaction.options.getSubcommand() === "check"){
-            logger.info(`'/rolememory check' was called by ${interaction.user.tag}`)
-            let status = await rolememory_db.check(interaction.guildId)
-            let msg = status == 1
-            ? "Role memory is turned on on this server"
-            : "Role memory is turned off on this server"
-            await interaction.reply({ content: msg, flags: MessageFlags.Ephemeral})
-        }
-    }
-}
+	data,
+	async execute(interaction) {
+		if (interaction.options.getSubcommand() === 'toggle') {
+			logger.info(`'/rolememory toggle' was called by ${interaction.user.tag}`);
+			const status = await rolememory_db.check(interaction.guildId);
+			const msg = status == 1
+				? 'Role memory has been turned off for this server'
+				: 'Role memory has been turned on for this server';
+			await rolememory_db.toggle(interaction.guildId);
+			await interaction.reply({ content: msg, flags: MessageFlags.Ephemeral });
+		}
+		else if (interaction.options.getSubcommand() === 'check') {
+			logger.info(`'/rolememory check' was called by ${interaction.user.tag}`);
+			const status = await rolememory_db.check(interaction.guildId);
+			const msg = status == 1
+				? 'Role memory is turned on on this server'
+				: 'Role memory is turned off on this server';
+			await interaction.reply({ content: msg, flags: MessageFlags.Ephemeral });
+		}
+	},
+};
--- commands/admin/starboard.js
+++ commands/admin/starboard.js
@@ -1,54 +1,57 @@
-const { SlashCommandBuilder, channelMention, PermissionFlagsBits, MessageFlags } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } = require('discord.js');
 const { StarboardSettingsDB } = require('../../db/starboard');
 const logger = require('../../utility/logger');
 
 const starboardsettings_db = new StarboardSettingsDB();
 
 const data = new SlashCommandBuilder()
-     .setName('starboard')
-     .setDescription("Starboard related settings")
-     .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
-     .addSubcommand(subcommand =>
-        subcommand
-             .setName("setthreshold")
-             .setDescription("Set the threshold for starboard")
-             .addIntegerOption(option => 
-                option
-                    .setName("threshold")
-                    .setDescription("The threshold to set it to")
-                    .setRequired(true)
-                )
-     )
-     .addSubcommand(subcommand =>
-        subcommand
-             .setName("getthreshold")
-             .setDescription("get the threshold for starboard")
-     );
+	.setName('starboard')
+	.setDescription('Starboard related settings')
+	.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName('setthreshold')
+			.setDescription('Set the threshold for starboard')
+			.addIntegerOption(option =>
+				option
+					.setName('threshold')
+					.setDescription('The threshold to set it to')
+					.setRequired(true),
+			),
+	)
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName('getthreshold')
+			.setDescription('get the threshold for starboard'),
+	);
 
 module.exports = {
-    data,
-    async execute(interaction) {
-        if (interaction.options.getSubcommand() === 'setthreshold'){
-            const threshold = interaction.options.getInteger('threshold');
-            logger.info(`'/starboard setthreshold' was called by ${interaction.user.tag}. Threshold now: ${threshold}`)
-            if (!await starboardsettings_db.check(interaction.guildId)){
-                await starboardsettings_db.add(interaction.guildId, 0, threshold);
-            }else{
-                await starboardsettings_db.updateThreshold(interaction.guildId, threshold);
-            }
-            await interaction.reply({ content: `Starboard threshold set to ${threshold}`, flags: MessageFlags.Ephemeral });
-        }else if(interaction.options.getSubcommand() === 'getthreshold'){
-            logger.info(`'/starboard getthreshold' was called by ${interaction.user.tag}.`)
-            var threshold = 0
-            if (!await starboardsettings_db.check(interaction.guildId)){
-                await starboardsettings_db.add(interaction.guildId, 0, 5)
-                threshold = 5;
-            }else{
-                threshold = await starboardsettings_db.getThreshold(interaction.guildId)
-            }
-            
-            await interaction.reply({ content:`Current Starboard threshold is ${threshold}`, flags: MessageFlags.Ephemeral })
-            logger.info(`'/starboard getthreshold' result: ${threshold}`)
-        }
-    }
+	data,
+	async execute(interaction) {
+		if (interaction.options.getSubcommand() === 'setthreshold') {
+			const threshold = interaction.options.getInteger('threshold');
+			logger.info(`'/starboard setthreshold' was called by ${interaction.user.tag}. Threshold now: ${threshold}`);
+			if (!await starboardsettings_db.check(interaction.guildId)) {
+				await starboardsettings_db.add(interaction.guildId, 0, threshold);
+			}
+			else {
+				await starboardsettings_db.updateThreshold(interaction.guildId, threshold);
+			}
+			await interaction.reply({ content: `Starboard threshold set to ${threshold}`, flags: MessageFlags.Ephemeral });
+		}
+		else if (interaction.options.getSubcommand() === 'getthreshold') {
+			logger.info(`'/starboard getthreshold' was called by ${interaction.user.tag}.`);
+			let threshold = 0;
+			if (!await starboardsettings_db.check(interaction.guildId)) {
+				await starboardsettings_db.add(interaction.guildId, 0, 5);
+				threshold = 5;
+			}
+			else {
+				threshold = await starboardsettings_db.getThreshold(interaction.guildId);
+			}
+
+			await interaction.reply({ content:`Current Starboard threshold is ${threshold}`, flags: MessageFlags.Ephemeral });
+			logger.info(`'/starboard getthreshold' result: ${threshold}`);
+		}
+	},
 };
