7be591863812a5aac7e28f1d739797fcab275c47
Jonathan Thorne <jon@jonthorne.com>
2025-10-02 19:38:30 -0400

    admin command to migrate databases from old version to new

---
+++ commands/admin/database.js
@@ -0,0 +1,35 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { ModboardDB, StarboardDB, StarboardSettingsDB } = require('../../db/starboard');
+const ArchivalDB = require('../../db/archival');
+const BirthdayDB = require('../../db/birthday');
+const { RoleMemoryDB, RoleDB } = require('../../db/rolememory');
+const SelfPromoMsgDB = require('../../db/selfpromo');
+
+const modboardDB = new ModboardDB();
+const starboardDB = new StarboardDB();
+const starboardSettingsDB = new StarboardSettingsDB();
+const archivalDB = new ArchivalDB();
+const birthdayDB = new BirthdayDB();
+const roleDB = new RoleDB();
+const roleMemoryDB = new RoleMemoryDB();
+const selfPromoMsgDB = new SelfPromoMsgDB();
+
+const data = new SlashCommandBuilder()
+	.setName('updatedb')
+	.setDescription('Updates db from integer version to string version')
+	.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels);
+
+module.exports = {
+	data,
+	async execute(interaction) {
+		await modboardDB.migrate();
+		await starboardDB.migrate();
+		await starboardSettingsDB.migrate();
+		await archivalDB.migrate();
+		await birthdayDB.migrate();
+		await roleDB.migrate();
+		await roleMemoryDB.migrate();
+		await selfPromoMsgDB.migrate();
+		interaction.reply('Database migrated');
+	},
+};
