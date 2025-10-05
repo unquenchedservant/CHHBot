2f518c143d6c501abbe285facd5b336527c2de80
Jonathan Thorne <jon@jonthorne.com>
2025-10-04 22:50:27 -0400

    test a ping for announcements

---
+++ commands/admin/anntest.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder, PermissionFlagsBits, MessageFlags } = require('discord.js');
+const config = require('../../utility/config');
+
+const data = new SlashCommandBuilder()
+    .setName('testPingAnnouncements')
+    .setDescription('Have the bot ping announcements channel')
+    .setDefaultMemberPermission(PermissionFlagsBits.ManageChannels);
+
+module.exports = {
+    data,
+    async execute(interaction){
+        const annch = interaction.client.channels.fetch(config.get_announcements_channel_id());
+        await annch.send("This is a test, sorry");
+    }
+}
