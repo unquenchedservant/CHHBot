69a3bf9203f434c94722ad09243c416b834a3d04
Jonathan Thorne <jon@jonthorne.com>
2025-10-21 20:16:10 -0400

    further fixes to db file

--- db/database.js
+++ db/database.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-empty-function */
 const sqlite3 = require('sqlite3').verbose();
 const logger = require('../utility/logger');
 
@@ -5,41 +6,40 @@ class Database {
   async execute(query, params = []) {
     return new Promise((resolve, reject) => {
       const db = new sqlite3.Database('chh.db', (openErr) => {
-        logger.error(`Failed to open DB: ${openErr}`);
-        return reject(openErr);
+        if (openErr) {
+          logger.error(`Failed to open DB: ${openErr}`);
+          return reject(openErr);
+        }
+
+        const trimmed = query.trim().toUpperCase();
+        if (trimmed.startsWith('SELECT')) {
+          db.all(query, params, (err, rows) => {
+            if (err) {
+              logger.error(`Database error: ${err}`);
+              db.close(() => {});
+              return reject(err);
+            }
+            db.close((closeErr) => {
+              if (closeErr) logger.error(`Error closing DB: ${closeErr}`);
+              resolve(rows);
+            });
+          });
+        }
+        else {
+          db.run(query, params, function(err) {
+            if (err) {
+              logger.error(`Database Error: ${err}`);
+              db.close(() => {});
+              return reject(err);
+            }
+            const result = { lastID: this.lastID, changes: this.changes };
+            db.close((closeErr) => {
+              if (closeErr) logger.error(`Error closing DB: ${closeErr}`);
+              resolve(result);
+            });
+          });
+        }
       });
-
-      const trimmed = query.trim().toUpperCase();
-      if (trimmed.startsWith('SELECT')) {
-        db.all(query, params, (err, rows) => {
-          if (err) {
-            logger.error(`Database error: ${err}`);
-            // eslint-disable-next-line no-inline-comments
-            db.close(() => { /* empty */ });
-            return reject(err);
-          }
-          db.close((closeErr) => {
-            if (closeErr) logger.error(`Error closing DB: ${closeErr}`);
-            resolve(rows);
-          });
-        });
-      }
-      else {
-        db.run(query, params, function(err) {
-          if (err) {
-            logger.error(`Database Error: ${err}`);
-            // eslint-disable-next-line no-empty-function
-            db.close(() => {});
-            return reject(err);
-          }
-          const result = { lastID: this.lastID, changes: this.changes };
-          db.close((closeErr) => {
-            if (closeErr) logger.error(`Error closing DB: ${closeErr}`);
-            resolve(result);
-          });
-
-        });
-      }
     });
   }
 
