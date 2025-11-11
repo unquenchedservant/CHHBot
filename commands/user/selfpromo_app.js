93a00095ff479a0cad0986dc92b1b17b48f2850a
Jon Thorne <chill@chillhumanoid.dev>
2025-11-10 20:34:37 -0500

    Fix bug that didn't tag users correctly in report channel

--- commands/user/selfpromo_app.js
+++ commands/user/selfpromo_app.js
@@ -38,7 +38,7 @@ module.exports = {
       .setDescription(msg);
 
     const reportChannel = await interaction.client.channels.cache.get(config.reportID);
-    const reportMsg = `The following message was tagged for self-promotion by <@${interaction.user.id}:\n${interaction.targetMessage.url}\n`;
+    const reportMsg = `The following message was tagged for self-promotion by <@${interaction.user.id}>:\n${interaction.targetMessage.url}\n`;
     logger.info(`${interaction.user.name} used the report self promo app command`);
     await interaction.targetMessage.reply({ embeds: [embed] });
     await interaction.reply({ content: 'Thanks, we let the user know about our self-promotion rules', flags: MessageFlags.Ephemeral });
